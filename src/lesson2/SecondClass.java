723f6618e26dfb694d34e9cc67feee79a4fedc63
vikas-atkari <vikasatkari@yahoo.in>
2018-12-22 13:00:28 +0530

    Added package lesson2 ...

---
+++ src/lesson2/SecondClass.java
@@ -0,0 +1,10 @@
+package lesson2;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second class to say Welcome in Java World !......");
+	}
+
+}
