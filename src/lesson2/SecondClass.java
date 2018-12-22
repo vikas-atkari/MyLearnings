fadf631fea5384c87e7f32a534184cf4ba933ab4
vikas-atkari <vikasatkari@yahoo.in>
2018-12-22 13:46:07 +0530

    added data types variables and printed ...

--- src/lesson2/SecondClass.java
+++ src/lesson2/SecondClass.java
@@ -3,8 +3,24 @@ package lesson2;
 public class SecondClass {
 
 	public static void main(String[] args) {
+		byte my_byte = 1;
+		short my_short = 12;
+		int my_int = 123;
+		long my_long = 2356;
+		float my_float = (float)4.5;
+		double my_double = 11.5;
+		char my_char = 'A';
+		boolean is_true = false;
 		// TODO Auto-generated method stub
 		System.out.println("Second class to say Welcome in Java World !......");
+		System.out.println(my_byte);
+		System.out.println(my_short);
+		System.out.println(my_int);
+		System.out.println(my_long);
+		System.out.println(my_float);
+		System.out.println(my_double);
+		System.out.println(my_char);
+		System.out.println(is_true);
 	}
 
 }
