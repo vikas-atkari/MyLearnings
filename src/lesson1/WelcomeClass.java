17f23fc5030b97be1979ff16f097eb166be6201f
vikas-atkari <vikasatkari@yahoo.in>
2018-12-23 11:47:23 +0530

    Scanner class is added.

--- src/lesson1/WelcomeClass.java
+++ src/lesson1/WelcomeClass.java
@@ -1,7 +1,17 @@
 package lesson1;
 
+import java.util.Scanner;
+
 public class WelcomeClass {
 public static void main(String[] args) {
 	System.out.println("Welcome to Java World !");
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter some number !");
+	int entered_value = sc.nextInt();
+	System.out.println("Entered value is:");
+	System.out.println(entered_value);
+	
+	
+	
 }
 }
