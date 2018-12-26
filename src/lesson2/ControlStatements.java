3d1814f6b7f5cef0412a40000f7659786eba9578
vikas-atkari <vikasatkari@yahoo.in>
2018-12-26 21:38:25 +0530

    Added new class ControlStatements

---
+++ src/lesson2/ControlStatements.java
@@ -0,0 +1,25 @@
+package lesson2;
+
+public class ControlStatements {
+
+	/*
+	 * if(boolean_expression) {
+	 * 
+	 * execute if the boolean expression is true.
+	 * 
+	 * }
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+
+		if (x == 10) {
+
+			System.out.println("Yes x == 10");
+
+		}
+	}
+
+}
---
+++ src/lesson2/IncrDecrOperator.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class IncrDecrOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int x = 10;
+        x += 5 ;// x = x + 5  put in place of +:: - * / %
+        System.out.println("Value of X :"+x);
+	}
+
+}
