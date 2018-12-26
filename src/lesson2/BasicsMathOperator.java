3749d5744d69c8020f6919d2a150f9aa41cdf6e2
vikas-atkari <vikasatkari@yahoo.in>
2018-12-26 21:04:54 +0530

    added comment // + - * / %

--- src/lesson2/BasicsMathOperator.java
+++ src/lesson2/BasicsMathOperator.java
@@ -4,11 +4,11 @@ public class BasicsMathOperator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x,y,result;
-		x = 20;
+		int x, y, result;
+		x = 70;
 		y = 30;
-		result = x + y;
-		System.out.println("Result :"+result);
+		result = x % y;  // + - * / % 
+		System.out.println("Result :" + result);
 
 	}
 
